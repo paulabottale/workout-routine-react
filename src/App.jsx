b410e3d5a46259f92a43f6b165620460bd77b779
paulabottale.dev <paulabottale.dev@gmail.com>
2026-04-07 14:07:25 -0300

    feat: add form with controlled inputs using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,52 @@
-
+import { useState } from 'react'
 
 function App() {
+
+  const [formData, setFormData] = useState({
+    personName:"",
+    personAge:"",
+    workoutLevel:"",
+    workoutTime:""
+  })
 
   return (
     <div>
       <h1>Smart Workout Planner</h1>
+
+      <input 
+      type="text"
+      placeholder="Your name"
+      value={formData.personName}
+      onChange={(e) => setFormData({...formData,personName: e.target.value})}
+      />
+
+      <input 
+      type="number"
+      placeholder="Your age"
+      value={formData.personAge}
+      onChange={(e) => setFormData({...formData,personAge: e.target.value})}
+      />
+
+      <select
+        value={formData.workoutLevel}
+        onChange={(e) => setFormData({...formData,workoutLevel: e.target.value})}
+      >
+        <option value="">Select level</option>
+        <option value="beginner">Beginner</option>
+        <option value="intermediate">Intermediate</option>
+        <option value="advanced">Advanced</option>
+      </select>
+
+      <input 
+      type="number"
+      placeholder="Workout time (minutes)"
+      value={formData.workoutTime}
+      // ...formData es un spread operator, lo que hace es copiar todos los valores actuales del objeto y solo pisar el campo que cambió. Si no lo hicieras, cada vez que actualizás un campo perderías los demás
+      onChange={(e) => setFormData({...formData,workoutTime: e.target.value})}
+      />
+
+      <button onClick={() => console.log(formData)}>Generate Routine</button>
+
     </div>
     )
 }
